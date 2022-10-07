43eb17c92e31aa9f992953ce9466e47ae17610bf
moretall <seongho4996@naver.com>
2022-10-07 21:45:33 +0900

    recursive

--- app.js
+++ app.js
@@ -9,26 +9,26 @@ export class App {
    */
   constructor({ apiService }) {
     this.apiService = apiService;
+    this.current = Date.now();
   }
 
   run = async () => {
-    const current = Date.now();
-    while(true){
-      await Promise.all([]);
-      await Promise.all([]);
-      await Promise.all([]);
+    await Promise.all([]);
+    await Promise.all([]);
+    await Promise.all([]);
 
-      const result = await this.apiService.match([]);
-      console.log(Date.now() - current);
-      console.log(result)
-      if (result.status === "finished") {
-        const score = await this.apiService.getScore();
-        const here = Date.now();
-        console.log(here - current);
-        console.log(score);
-        return;
-      }
+    const result = await this.apiService.match([]);
+    console.log(Date.now() - this.current);
+    console.log(result)
+    if (result.status === "finished") {
+      const score = await this.apiService.getScore();
+      const here = Date.now();
+      console.log(here - this.current);
+      console.log(score);
+      return;
     }
+
+    this.run();
   };
 
 }
