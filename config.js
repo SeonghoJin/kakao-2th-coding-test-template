e27fc1c4aedd51d28ee574d08e90e9113aba2167
moretall <seongho4996@naver.com>
2022-10-29 19:34:19 +0900

    remove config

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
  * }}
  */
 export const config = {
-  authToken: "d810411b51d8bbc29f28d91420f8e97d",
-  baseURL: "https://68ecj67379.execute-api.ap-northeast-2.amazonaws.com/api",
+  authToken: "",
+  baseURL: "",
   problem: 1,
 };
